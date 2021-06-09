f943ef6d5e2970e2f68a1c0a367d9b7e0fffbc9a
ivobul <24499940+ivobul@users.noreply.github.com>
2021-06-09 14:50:05 -0400

    Add files via upload

---
+++ digital-clock/script.js
@@ -0,0 +1,54 @@
+function displayTime() {
+  const currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+
+  // Get the weekday
+  const options = {weekday: 'long'};
+  const getDay = currentTime.toLocaleTimeString('en-us', options).split(' ')[0];
+
+  // This gets a "handle" to the clock div in our HTML
+  const clockDiv = document.getElementById('clock');
+
+  // Let's set the AM and PM meridiem and 
+  // 12-hour format
+  let meridiem = "AM";  // Default is AM
+  // Convert from 24 hour to 12 hour format
+  // and keep track of the meridiem.
+  if (hours > 12) {
+      hours = hours - 12;
+      meridiem = "PM";
+  }
+
+  // 0 AM and 0 PM should read as 12
+  if (hours === 0) {
+      hours = 12;    
+  }
+
+  // If the seconds digit is less than ten                    
+  if (hours < 10) {
+    // Add the "0" digit to the front
+    // so 9 becomes "09"
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    // Add the "0" digit to the front
+    // so 9 becomes "09"
+    minutes = `0${minutes}`;
+  }
+
+  if (seconds < 10) {
+    // Add the "0" digit to the front
+    // so 9 becomes "09"
+    seconds = `0${seconds}`;
+  }
+
+  
+  // Set the text inside the clock div
+  // to the hours, minutes, and seconds of the current time
+  clockDiv.innerText = `${getDay} ${hours}:${minutes}:${seconds} ${meridiem}`;
+}
+
+setInterval(displayTime, 1000);
