fba777f1bdf849a8ed9402fc06bf4bcec19670e4
ivobul <24499940+ivobul@users.noreply.github.com>
2021-06-09 14:54:06 -0400

    Add files via upload

---
+++ popup-penguins/script.js
@@ -0,0 +1,25 @@
+const popupItems = ["penguin1", "penguin2", "penguin3", "penguin4", "penguin5", "penguin6", "penguin7", "penguin8", "yeti"];
+ 
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+
+  let spliced = popupItems.splice([Math.floor(Math.random() * popupItems.length)], 1);
+   
+  box.classList.add(spliced);
+
+  if (box.classList.contains("yeti")) { 
+
+    box.addEventListener("mousedown", () => {
+
+      box.style.backgroundImage = 'url("penguin_pngs/yeti.png")';
+
+      if (!alert("Yaaaarrrr!")) {
+        window.location.reload();
+      }
+
+    });
+    
+  }
+
+});
